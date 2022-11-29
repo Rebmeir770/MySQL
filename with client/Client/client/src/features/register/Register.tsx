71f2a77dfc7b1bf729214b39bdd9dc809fcf1ed9
Meir Duvid Krykun <97189724+Rebmeir770@users.noreply.github.com>
2022-11-29 16:59:58 +0200

    update Register.tsx

--- with client/Client/client/src/features/register/Register.tsx
+++ with client/Client/client/src/features/register/Register.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
 const Register = () => {
+  function handleRegister(ev:any){
+    ev.preventdefault();
+    console.log('register')
+    let {email, password, name} = ev.targe.elements;
+    email = email.value;
+    password = password.value;
+    name = name.value;
+
+    console.log(email, password, name)
+  }
   return (
-    <div>Register</div>
+    <div>
+       <form onSubmit={handleRegister}>
+        <input type="email" name="email" placeholder='email'/>
+        <input type="text" name="name" placeholder='name'/>
+        <input type="password" name="password" placeholder='password'/>
+        <button type='submit'>Register</button>
+       </form>
+    </div>
   )
 }
 
