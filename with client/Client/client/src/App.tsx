b9905596811f61c84f0903d6c2ae82c142324f41
Meir Duvid Krykun <97189724+Rebmeir770@users.noreply.github.com>
2022-12-04 13:37:47 +0200

    Login - Register

--- with client/Client/client/src/App.tsx
+++ with client/Client/client/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.svg'
 import { Counter } from './features/counter/Counter';
 import './App.css';
-import Register from './features/register/Register';
+import Register from './features/Register/register';
+import Login from './features/Login/login';
 
 function App() {
   return (
     <div>
+      <Login/>
       <Register/>
     </div>
   );
