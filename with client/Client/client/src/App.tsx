2079e136f2faa6e49f016357223de6104211e7c9
Meir Duvid Krykun <97189724+Rebmeir770@users.noreply.github.com>
2022-12-11 20:05:57 +0200

    wip app.tsx

--- with client/Client/client/src/App.tsx
+++ with client/Client/client/src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css';
 import Register from './features/Register/register';
 import Login from './features/Login/login';
 import Main from './features/Main/main';
 import Home from './features/Home/home';
-import Songs from './features/Musics/indexSongs';
-import Musics from './features/Music/indexMusic';
-import './styles.scss';
+
+
 
 
 const App = () => {
@@ -15,9 +13,11 @@ const App = () => {
     <BrowserRouter>
         <Routes>
            <Route path="/" element={<Main />} >
+
                <Route index element={<Home />} />
                <Route path="Login"index element={<Login/>} />
                <Route path="Register" element={<Register/>} />
+
           </Route>
            
         </Routes>
