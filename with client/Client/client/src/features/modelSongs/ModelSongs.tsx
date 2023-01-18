421983da355fa1c3e7e33a2105d88cec17d35a10
Meir Duvid Krykun <97189724+Rebmeir770@users.noreply.github.com>
2023-01-18 13:07:49 +0200

    wip

---
+++ with client/Client/client/src/features/modelSongs/ModelSongs.tsx
@@ -0,0 +1,86 @@
+import path from 'path';
+import React from 'react'
+import {useState, useEffect} from 'react';
+
+
+
+const ModelSongs = () => {
+    const [songs] = useState([
+        {
+          title: "ohtotoh",
+          artist: "Avrham Fried",
+          img_src: "./images/avrham_fried.jpg",
+          src: "./music/avrham_fried_ohtotoh.mp3"
+        },
+        {
+          title: "Song 2",
+          artist: "Nachas",
+          img_src: "./images/ba_elim-hashem-cover.jpg",
+          src: "./music/ba_eilim_hashem.mp3"
+        },
+        {
+          title: "Song 3",
+          artist: "Mordechai ben David",
+          img_src: "./images/boiee_MBD.jpg",
+          src: "./music/boiee_beshalom.mp3"
+        },
+        {
+            title: "Song 4",
+            artist: "Boruch Sholom Blesofsky",
+            img_src: "./images/Ribon.jpg",
+            src: "./music/ribon_1.mp3"
+        },
+        {
+            title: "Song 5",
+            artist: "Boruch Sholom Blesofsky",
+            img_src: "./images/Ribon.jpg",
+            src: "./music/ribon_1.mp3"
+        },
+        {
+            title: "Song 6",
+            artist: "Mordechai ben David",
+            img_src: "./images/hashatu-huchu.jpg",
+            src: "./music/hashatu-huchu.mp3"
+        },
+        {
+            title: "Song 7",
+            artist: "Mordechai ben David",
+            img_src: "./images/rabbi_shimon_mbd.jpg",
+            src: "./music/rabbi_shimon_mbd.mp3"
+        },
+        {
+            title: "Song 8",
+            artist: "Shalom Goldstein",
+            img_src: "./images/alehkatan.jpg",
+            src: "./music/alekatan.mp3"
+        }
+       
+    ]);
+
+    const [currentSongIndex, setCurrentSongIndex] = useState(0);
+    const [nextSongIndex, setNextSongIndex] = useState(0);
+  
+    useEffect(() => {
+      setNextSongIndex(() => {
+        if (currentSongIndex + 1 > songs.length - 1) {
+          return 0;
+        } else {
+          return currentSongIndex + 1;
+        }
+      });
+    }, [currentSongIndex]);
+    
+
+  return (
+    <div>
+{/* <Player 
+        currentSongIndex={currentSongIndex} 
+        setCurrentSongIndex={setCurrentSongIndex} 
+        nextSongIndex={nextSongIndex} 
+        songs={songs}
+      /> */}
+    </div>
+  )
+}
+
+export default ModelSongs
