3848fa834bcdec98871c864b9d2f053af6c69dc3
Meir Duvid Krykun <97189724+Rebmeir770@users.noreply.github.com>
2023-01-19 01:00:26 +0200

    Update Home.tsx

--- with client/Client/client/src/features/Home/home.tsx
+++ with client/Client/client/src/features/Home/home.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import '../styles/home.scss';
 import ModelSongs from "../modelSongs/ModelSongs";
-import { useAppDispatch } from "../../app/hooks";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { useNavigate } from "react-router-dom";
 import { logoutAsync } from "../User/userApi";
+import { selectUser } from "../User/logoutSlice";
 
 function Home() {
   const dispatch = useAppDispatch();
+  const user = useAppSelector(selectUser);
   const navigate = useNavigate();
 
   const logout = () => {
