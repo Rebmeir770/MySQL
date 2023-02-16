ed784a59d907d82cd676df5986b0770440ef5623
Meir Duvid Krykun <97189724+Rebmeir770@users.noreply.github.com>
2023-02-16 22:42:09 +0200

    Update Navbar.tsx and scss

--- Fiver/Fiver/src/App.tsx
+++ Fiver/Fiver/src/App.tsx
@@ -10,6 +10,10 @@ function App() {
   return (
     <div className="App">
       <NavBar />
+      {/* <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste enim facere modi recusandae ullam sunt quos harum, quis tempore. Expedita odio consequuntur commodi quibusdam mollitia amet consectetur dolores neque optio.</h1>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore sapiente fuga dolorum odit quam ipsum voluptas? Magnam libero voluptates quidem omnis quasi ea. Odit perspiciatis voluptatibus sed totam deleniti mollitia!</h1>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non fuga fugit labore voluptatibus quos incidunt doloribus magni, consequuntur libero ex dignissimos, amet nam ipsum animi itaque minus illo accusantium maiores.</h1>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro hic a reprehenderit iure quas maxime. Accusantium alias, minus quisquam illo adipisci non asperiores laborum rem consectetur. Ipsum quasi debitis soluta!</h1> */}
     </div>
   )
 }
--- Fiver/Fiver/src/components/NavBar.tsx
+++ Fiver/Fiver/src/components/NavBar.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './NavBar.scss';
 import { Link } from 'react-router-dom';
 
+const [active, setActive] = useState<boolean>(false);
+
 const NavBar = () => {
+  
+  const isActive = () =>{
+    window.scrollY > 0 ? setActive(true) : setActive(false)
+  }
+
+  useEffect(()=>{
+    window.addEventListener("scroll", isActive);
+    
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    } 
+  },[])
+
   return (
-    <div className='navbar'>
+    <div className={active ? "navabar active" : 'navbar'}>
         <div className="container">
             <div className="logo">
-              <Link to="/">
+              {/* <Link to="/"> */}
                 <span className='text'>Fiverr</span>
-              </Link>  
+              {/* </Link>   */}
                 <span className='dot'>.</span>
             </div>
             <div className="links">
@@ -22,13 +37,17 @@ const NavBar = () => {
             </div>
             
         </div>
-        <hr/>
-            <div className="menu">
-              <span>Test</span>
-              <span>Test2</span>
-            </div>
+         {active && ( 
+            <>
+              <hr />
+              <div className="menu">
+                <span>Test</span>
+                <span>Test2</span>
+              </div>
+           </>
+         )}
     </div>
-  )
-}
+  );
+};
 
 export default NavBar;
